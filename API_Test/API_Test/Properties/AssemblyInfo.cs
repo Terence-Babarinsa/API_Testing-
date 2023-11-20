677475570812ba29d5122a6e9a6f7095e523b1d9
Terence-Babarinsa <69308628+Terence-Babarinsa@users.noreply.github.com>
2023-11-20 17:23:21 +0000

    Setup

---
+++ API_Test/API_Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("API_Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("API_Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3147a596-2dc3-4331-8a34-1f4ca123ca00")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ API_Test/API_Test/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace API_Test
+{
+    [TestClass]
+    public class APITest
+    {
+
+        private HttpClient httpClient;
+        [TestInitialize]
+        public void Setup()
+        {
+            httpClient = new HttpClient();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
